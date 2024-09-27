3ca0016e18c6dde07459db7f79b442f64e3b3c3d
Ishan Bhargava <ishan1939.be21@chitkara.edu.in>
2024-09-27 19:02:59 +0530

    Add camera tracking

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] float smoothTime;
+
+    Vector3 relativePos = Vector3.zero;
+    Vector3 smoothVel = Vector3.zero;
+
+    void Start() {
+        relativePos = transform.position - target.position;
+    }
+
+    void Update() {
+    }
+
+    private void FixedUpdate() {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + relativePos, ref smoothVel, smoothTime);
+    }
+
+}
