a56e666ed02c4b80b947850b0f38e33fd1bf3ea0
Ishan Bhargava <ishan1939.be21@chitkara.edu.in>
2024-09-29 00:01:43 +0530

    Add combat director

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,6 +4,7 @@ public static class GameGlobal {
 
     public static GameObject Player;
     public static Camera Cam;
+    public static Transform EnemyHolder;
     public static Transform ProjectileHolder;
 
 }
@@ -12,11 +13,13 @@ public class Game : MonoBehaviour {
 
     [SerializeField] GameObject Player;
     [SerializeField] Camera Cam;
+    [SerializeField] Transform EnemyHolder;
     [SerializeField] Transform ProjectileHolder;
 
     void Start() {
         GameGlobal.Player = Player;
         GameGlobal.Cam = Cam;
+        GameGlobal.EnemyHolder = EnemyHolder;
         GameGlobal.ProjectileHolder = ProjectileHolder;
     }
 
